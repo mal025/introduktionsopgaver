9054edb2e83cff406faa308c91d5d0eee69bc417
mal025 <mal025@edu.zealand.dk>
2025-02-14 22:24:37 +0100

    Add files via upload

---
+++ LottoTal/LottoTal/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LottoTal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] lottotal = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                lottotal[i] = random.Next(1,36);
+            }
+
+            Array.Sort(lottotal);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"{lottotal[i]} ");
+            }
+        }
+}
+}
